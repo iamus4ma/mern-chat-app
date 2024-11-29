d26a8160855921dd3dc349f7e88a9d6d08a63d3a
iamus4ma <iamus4ma@gmail.com>
2024-11-29 18:14:14 +0500

    live

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -6,45 +6,38 @@ import io from "socket.io-client";
 const SocketContext = createContext();
 
 export const useSocketContext = () => {
-  return useContext(SocketContext);
+	return useContext(SocketContext);
 };
 
 export const SocketContextProvider = ({ children }) => {
-  const [socket, setSocket] = useState(null);
-  const [onlineUsers, setOnlineUsers] = useState([]);
+	const [socket, setSocket] = useState(null);
+	const [onlineUsers, setOnlineUsers] = useState([]);
   const authUser = useSelector((state) => state.user);
-  // const { authUser } = useAuthContext();
+	// const { authUser } = useAuthContext();
 
-  useEffect(() => {
-    if (authUser) {
-      const socket = io(
-        "https://chat-kro.onrender.com" || "http://localhost:8000",
-        {
-          query: {
-            userId: authUser._id,
-          },
-        }
-      );
+	useEffect(() => {
+		if (authUser) {
+			const socket = io("https://chat-kro.onrender.com", {
+				query: {
+					userId: authUser._id,
+				},
+			});
 
-      setSocket(socket);
+			setSocket(socket);
 
-      // socket.on() is used to listen to the events. can be used both on client and server side
-      socket.on("getOnlineUsers", (users) => {
-        setOnlineUsers(users);
-      });
+			// socket.on() is used to listen to the events. can be used both on client and server side
+			socket.on("getOnlineUsers", (users) => {
+				setOnlineUsers(users);
+			});
 
-      return () => socket.close();
-    } else {
-      if (socket) {
-        socket.close();
-        setSocket(null);
-      }
-    }
-  }, [authUser]);
+			return () => socket.close();
+		} else {
+			if (socket) {
+				socket.close();
+				setSocket(null);
+			}
+		}
+	}, [authUser]);
 
-  return (
-    <SocketContext.Provider value={{ socket, onlineUsers }}>
-      {children}
-    </SocketContext.Provider>
-  );
+	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
